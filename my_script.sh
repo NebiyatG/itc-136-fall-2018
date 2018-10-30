a7dc556cdf4f02fb03efcf30ae8620d8fe0183c3
Nebiyat <love.jesus.ng@gmail.com>
2018-10-29 21:06:26 -0700

    I added more stuff per Nicole said so

--- my_script.sh
+++ my_script.sh
@@ -5,3 +5,17 @@ echo "hello ITc-136"
 
 echo "edited in github"
 
+
+edho "This is your current working directory:"
+pwd
+
+
+eco "this is the version of linux you're running:"
+uname -a
+
+
+echo "These are your logged in users:"
+who
+
+
+
