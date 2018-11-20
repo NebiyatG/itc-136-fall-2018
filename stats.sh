81b0b902a4a2d09663a74f028ca42fddc21a654d
Yemale <44388338+Yemale@users.noreply.github.com>
2018-11-19 20:15:05 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-diskused=$(df -h | grep "/dev/sda1"/ | awk '{print $5}
-memfree= -mh |grep fr
-free -mh | mh | grep Nem: | awk '{print $4}' )
+diskused=$( df -h | grep "/dev/sda1"/ | awk '{print $5}' )
+
+memfree=$(free -mh | grep Mem: | awk '{print $4}' )
+
+
 connections=$( netstat | grep tcp )
-user$( who )
+
+
+users=$( who )
+
 echo "This is a snapshot of your current system:
       Disk Used: $diskused
       Free Memory: $memfree
@@ -14,12 +19,11 @@ echo "This is a snapshot of your current system:
  "
  
  echo "
- Colar info:
+ color info:
  http://mlsc.flogisoft.com/bash/tip_colors_and_formattint 
  http://wiki..bash-bash-hackers.org/scripting/style
  http://help.ubuntu.com/community/UnityLauncherAndDesktopFiles
-
-
+"
 
 
 
