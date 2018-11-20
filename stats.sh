06504530eec1773925a648c0b71bdf88f56443b9
Yemale <44388338+Yemale@users.noreply.github.com>
2018-11-19 20:22:18 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -2,7 +2,7 @@
 
 diskused=$( df -h | grep "/dev/sda1"/ | awk '{print $5}' )
 
-memfree=$(free -mh | grep Mem: | awk '{print $4}' )
+memfree=$(free -mh | grep Mem: | awk '{print $7}' )
 
 
 connections=$( netstat | grep tcp )
